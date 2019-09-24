59f15128e913fc6283b60b84e0556f088edd0586
Kalanda <kalanda@gmail.com>
2019-09-24 20:01:38 +0200

    Eslint added to CI

--- src/tags/asset_path.js
+++ src/tags/asset_path.js
@@ -2,9 +2,11 @@ const DEFAULT_ASSETS_BASE_URL = '/assets';
 
 // Usage: {% asset_path path %}
 const asset_path = {
+  // eslint-disable-next-line no-unused-vars
   parse: function(tagToken, remainTokens) {
     this.str = tagToken.args; // name
   },
+  // eslint-disable-next-line no-unused-vars
   render: function(scope, hash) {
     const assets_path = scope.environments.assets_base_url || DEFAULT_ASSETS_BASE_URL;
     return `${assets_path}/${this.str}`;
--- src/tags/page_url.js
+++ src/tags/page_url.js
@@ -1,9 +1,11 @@
 // Usage: {% page_url name [location] %}
 const page_url = {
+  // eslint-disable-next-line no-unused-vars
   parse: function(tagToken, remainTokens) {
     this.name = tagToken.args.split(' ')[0]; // name
     this.model = tagToken.args.split(' ')[1]; // model
   },
+  // eslint-disable-next-line no-unused-vars
   render: function(scope, hash) {
     return `/${this.name}`;
   }
