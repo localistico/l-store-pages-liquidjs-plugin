c3d4f0cc42291fb804673274944f864b503f3bd5
Kalanda <kalanda@gmail.com>
2019-10-09 19:45:39 +0200

    Allow to override assets path in asset_path tag

--- src/tags/asset_path.js
+++ src/tags/asset_path.js
@@ -1,4 +1,5 @@
 const DEFAULT_ASSETS_BASE_URL = '/assets';
+const DEFAULT_ASSETS_CONTENT_PATH = 'assets';
 
 const fs = require('fs');
 
@@ -10,10 +11,11 @@ const asset_path = {
   },
   // eslint-disable-next-line no-unused-vars
   render: function(scope, hash) {
-    const assets_path = scope.environments.assets_base_url || DEFAULT_ASSETS_BASE_URL;
+    const assets_base_url = scope.environments.assets_base_url || DEFAULT_ASSETS_BASE_URL;
+    const assets_path = scope.environments.assets_content_path || DEFAULT_ASSETS_CONTENT_PATH;
     const filepath = `${scope.opts.root[0]}/${assets_path}/${this.str}`;
     fs.readFileSync(filepath);
-    return `${assets_path}/${this.str}`;
+    return `${assets_base_url}/${this.str}`;
   }
 };
 
